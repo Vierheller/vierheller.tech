c61ea854bffa524348e1c701e30dedd5fe086c72
Jan-Niklas Vierheller <j.vierheller@outlook.de>
2017-10-20 01:14:30 +0200

    Added bootstrap and stylus

---
+++ output/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My Page</title>
+    </head>
+    <body>
+
+    <h1>My first PHP page</h1>
+
+    <?php
+    echo "Hello World!";
+    ?>
+
+    </body>
+</html>
