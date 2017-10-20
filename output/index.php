3c78996647ff249a687b1243dfbecc3be6ef30ad
Jan-Niklas Vierheller <j.vierheller@outlook.de>
2017-10-20 02:12:17 +0200

    100% viewport image

--- output/index.php
+++ output/index.php
@@ -2,8 +2,15 @@
 <html>
     <head>
         <title>My Page</title>
+
+        <link rel="stylesheet" href="css/lib/normalize/normalize.css">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/lib/bootstrap4/bootstrap.css">
     </head>
     <body>
+        <section id="header">
+            <img src="img/header-code.jpg">
+        </section>
 
     <h1>My first PHP page</h1>
 
