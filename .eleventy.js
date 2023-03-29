50ae88a0dc63abdb54e4a1bb7cb6caac52e3922c
U549950 <jan-niklas.vierheller@lhsystems.com>
2023-03-29 22:31:55 +0200

    fix: Path prefix for anchors

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,10 @@
 const eleventySass = require("eleventy-sass");
+const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
 
 module.exports = function(eleventyConfig) {
+  // Plugins
   eleventyConfig.addPlugin(eleventySass);
+  eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
 
   // Passthroughs  
   eleventyConfig.addPassthroughCopy("src/img");
